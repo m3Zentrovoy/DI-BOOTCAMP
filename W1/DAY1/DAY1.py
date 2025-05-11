9a992011a7c84b3a7a165249aa833134c7a312db
Maksim <tobokerowl69@gmail.com>
2025-05-11 14:35:58 +0300

    add_Xp_gold_ex

--- W1/DAY1/DAY1.py
+++ W1/DAY1/DAY1.py
@@ -104,3 +104,34 @@ print('Manishma Haverim!'.replace('Manishma', 'Pizdec').upper())
 # TEUDAT_ZEUT=
 
 #=======================================================
+
+
+
+# CONDITIONALS : IF STATMENTS
+
+# if condition:
+#     <block of code>
+
+# secret_nuber = 67
+# user_numbers = int(input('Gues a numer? '))
+# if user_numbers == (secret_nuber):
+#     print('Congadilation!')
+
+# elif user_numbers == 7:
+#     print('Oh thats my favorite number!')
+# else:
+#     print('Sorry, you wrong')
+
+
+
+# user_number = int(input('Write a number between 1 and 100 >>> '))
+# if user_number % 3  == 0 and user_number % 5 == 0:
+#     print('FizzBuzz')
+# elif user_number % 5 == 0:
+#     print('Buzz')
+# elif user_number % 3 == 0:
+#     print('Fizz')
+
+# else: 
+#     print('not valid')
+    
---
+++ W1/DAY1/exercise_XP_Gold.py
@@ -0,0 +1,16 @@
+# #ex1
+# print('Hello Word\n' * 4 + 'I love python\n' * 4)
+
+#ex2
+
+user_month = int(input('Write number of the month (1 to 12)'))
+
+if 3 <= user_month <= 5:
+    print('it is Spring')
+elif 6 <= user_month <= 8:
+       print('it is Summer')
+elif 9 <= user_month <= 11:
+       print('it is Autemn')
+else:
+       print('it is Winter')
+    
