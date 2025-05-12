b8b54881f1cfb5ea056ff5aba9f945a99e4695dd
Maksim <tobokerowl69@gmail.com>
2025-05-12 20:22:50 +0300

    done_daily

---
+++ W1/DAY2/Exercise/Daily Challenge.py
@@ -0,0 +1,29 @@
+
+#ex1
+
+
+
+# x = []
+# number = int(input('write the number (integer) '))
+# length = int(input('than write the length (integer) '))
+
+# for i in range(1, length + 1):
+
+#     x.append(number * i)
+
+# print(x)
+
+
+
+#ex2
+
+# word = input('Write a string ')
+# result = []
+# for char in word:
+#     if not result:
+#        result.append(char)
+#     elif result and char != result[-1]:
+#         result.append(char)
+    
+# print(''.join(result))
+
