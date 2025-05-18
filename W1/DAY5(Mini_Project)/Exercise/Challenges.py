6bd957739f6a2654b46ce50ac8521e30586e2a5d
Maksim <tobokerowl69@gmail.com>
2025-05-18 10:29:05 +0300

    addnewFolder

---
+++ W1/DAY5(Mini_Project)/Exercise/Challenges.py
@@ -0,0 +1,104 @@
+# #ex1 
+
+# # Создаём пустой список
+# item_list = []
+
+# # Функция для вставки элемента
+# def insert_item():
+#     item = input("Введите элемент: ")  # Запрашиваем элемент
+#     index = int(input("Введите индекс: "))  # Запрашиваем индекс
+#     item_list.insert(index, item)  # Вставляем элемент в список
+#     print("Список:", item_list)  # Показываем результат
+
+# # Вызываем функцию
+# insert_item()
+
+
+#ex2
+
+
+# user_input = input("Введите что-нибудь: ")
+# print(user_input.count(' '))
+
+
+#ex3
+
+
+# counter_sup = 0
+# counter_low = 0
+# user_input = input("Введите что-нибудь: ")
+
+# for char in user_input:
+#     if char.isupper():
+#         counter_sup += 1
+#     elif char.islower():
+#         counter_low += 1
+
+# print(f'number of upper - {counter_sup} and number of lower - {counter_low}')
+
+#ex4
+
+# numbers = ([1,5,4,2])
+
+# def fund_sum(numbers):
+#     total = 0
+#     for num in numbers:
+#         total += num
+#     return total
+    
+    
+# result = fund_sum(numbers)
+# print('Sum' ,result)
+
+
+#ex5
+
+# numbers = ([0,1,3,50,100])
+# box = [numbers[0]]
+# def find_max(numbers):
+#     for num in numbers:
+#         if num > box[0]:
+#             box[0] = num
+#     return box[0]
+        
+
+
+# result = (find_max(numbers))
+# print("Max:" , result)
+
+
+#ex6
+
+# n = int(input("Write your number "))
+
+# def factorial(n):
+#     if n < 0:
+#       print('not defined for negative numbers, write num > 0')
+
+#     elif n == 0 or n == 1:
+#       return 1
+#     result = 1
+#     for i in range(1, n + 1):
+#         result *= i
+#     return result
+    
+# factorial(7)
+# print(factorial(n))
+
+
+#ex7
+
+
+# user_list = input('Write you list (separeted space) ').split()
+
+# item = input('Write element for search ')
+
+# def count_lst(lst,element):
+#     count = 0
+#     for element in lst:
+#         if item == element:
+#             count += 1
+
+#     return count
+
+# print(count_lst(user_list, item))
--- W1/DAY5(Mini_Project)/Exercise/TicTacToe.py
+++ W1/DAY5(Mini_Project)/Exercise/TicTacToe.py
@@ -1,116 +1,116 @@
 
-# # Шаг 1: Представление игрового поля
-# field = [['▢' for _ in range(3)] for _ in range(3)]
+# Шаг 1: Представление игрового поля
+field = [['▢' for _ in range(3)] for _ in range(3)]
 
-# # Шаг 2: Отображение игрового поля
-# def display_board(field):
-#     for row_idx, row in enumerate(field):
-#         for col_idx, cell in enumerate(row):
-#             number = row_idx * 3 + col_idx + 1
-#             if cell == '▢':
-#                 print(number, end=" ")
-#             else:
-#                 print(cell, end=" ")
-#         print()
+# Шаг 2: Отображение игрового поля
+def display_board(field):
+    for row_idx, row in enumerate(field):
+        for col_idx, cell in enumerate(row):
+            number = row_idx * 3 + col_idx + 1
+            if cell == '▢':
+                print(number, end=" ")
+            else:
+                print(cell, end=" ")
+        print()
 
-# # Шаг 3: Получение ввода игрока
-# def player_input(player, field):
-#     while True:
-#         try:
-#             number = int(input(f"Player {player}, enter cell number (1-9): "))
-#             if number < 1 or number > 9:
-#                 print('Please enter a number between 1 and 9.')
-#                 continue
-#             row = (number - 1) // 3
-#             col = (number - 1) % 3
-#             if field[row][col] != '▢':
-#                 print('Cell is already taken!')
-#                 continue
-#             return (row, col)
-#         except ValueError:
-#             print('Please enter a valid number!')
+# Шаг 3: Получение ввода игрока
+def player_input(player, field):
+    while True:
+        try:
+            number = int(input(f"Player {player}, enter cell number (1-9): "))
+            if number < 1 or number > 9:
+                print('Please enter a number between 1 and 9.')
+                continue
+            row = (number - 1) // 3
+            col = (number - 1) % 3
+            if field[row][col] != '▢':
+                print('Cell is already taken!')
+                continue
+            return (row, col)
+        except ValueError:
+            print('Please enter a valid number!')
 
-# # Шаг 4: Проверка победы
-# def check_win(board, player):
-#     # Проверка строк
-#     for i in range(3):
-#         if all(board[i][j] == player for j in range(3)):
-#             return True
+# Шаг 4: Проверка победы
+def check_win(board, player):
+    # Проверка строк
+    for i in range(3):
+        if all(board[i][j] == player for j in range(3)):
+            return True
     
-#     # Проверка столбцов
-#     for i in range(3):
-#         if all(board[j][i] == player for j in range(3)):
-#             return True
+    # Проверка столбцов
+    for i in range(3):
+        if all(board[j][i] == player for j in range(3)):
+            return True
     
-#     # Проверка главной диагонали
-#     if all(board[i][i] == player for i in range(3)):
-#         return True
+    # Проверка главной диагонали
+    if all(board[i][i] == player for i in range(3)):
+        return True
     
-#     # Проверка побочной диагонали
-#     if all(board[i][2-i] == player for i in range(3)):
-#         return True
+    # Проверка побочной диагонали
+    if all(board[i][2-i] == player for i in range(3)):
+        return True
     
-#     return False
+    return False
 
-# # Шаг 5: Проверка ничьей
-# def check_tie(board):
-#     return not any('▢' in row for row in board)
+# Шаг 5: Проверка ничьей
+def check_tie(board):
+    return not any('▢' in row for row in board)
 
-# # Шаг 6: Основной игровой цикл
-# def play():
-#     board = [['▢' for _ in range(3)] for _ in range(3)]
-#     current_player = 'X'
-    
-#     while True:
-#         display_board(board)
-#         row, col = player_input(current_player, board)
-#         board[row][col] = current_player
-        
-#         if check_win(board, current_player):
-#             display_board(board)
-#             print(f"Player {current_player} wins!")
-#             break
-        
-#         if check_tie(board):
-#             display_board(board)
-#             print("It's a tie!")
-#             break
-        
-#         current_player = 'O' if current_player == 'X' else 'X'
-
-# # Запускаем игру
-# play()
-
-
-
-board_size = 3
-board = [1,2,3,4,5,6,7,8,9]
-
-def draw_board():
-    '''view draw board'''
-    print('_' * 4 * board_size)
-    for i in range(board_size):
-        print((' '* 3 + '|')* 3)
-        print('', board[i*3], '|' , board[1 + i*3], '|', board[2 + i*3], '|')
-        # print('_' * 4 * board_size)
-        print(('_'* 3 + '|')* 3)
-    
-    pass
-def game_step(index,char):
-    if (index > 9 or index < 1):
-  
-    pass
-def check_win():
-    pass
-
-def start_game():
+# Шаг 6: Основной игровой цикл
+def play():
+    board = [['▢' for _ in range(3)] for _ in range(3)]
     current_player = 'X'
-    step = 1
-    draw_board()
+    
+    while True:
+        display_board(board)
+        row, col = player_input(current_player, board)
+        board[row][col] = current_player
+        
+        if check_win(board, current_player):
+            display_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        
+        if check_tie(board):
+            display_board(board)
+            print("It's a tie!")
+            break
+        
+        current_player = 'O' if current_player == 'X' else 'X'
 
-    while (step < 10) and check_win() == False:
-        index = input('Step plyer')
+# Запускаем игру
+play()
+
+
+
+# board_size = 3
+# board = [1,2,3,4,5,6,7,8,9]
+
+# def draw_board():
+#     '''view draw board'''
+#     print('_' * 4 * board_size)
+#     for i in range(board_size):
+#         print((' '* 3 + '|')* 3)
+#         print('', board[i*3], '|' , board[1 + i*3], '|', board[2 + i*3], '|')
+#         # print('_' * 4 * board_size)
+#         print(('_'* 3 + '|')* 3)
+    
+#     pass
+# def game_step(index,char):
+#     if (index > 9 or index < 1):
+  
+#     pass
+# def check_win():
+#     pass
+
+# def start_game():
+#     current_player = 'X'
+#     step = 1
+#     draw_board()
+
+#     while (step < 10) and check_win() == False:
+#         index = input('Step plyer')
 
                       
-print("Welcome to TTT")
-start_game()
+# print("Welcome to TTT")
+# start_game()
