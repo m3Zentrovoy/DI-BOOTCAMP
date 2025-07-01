5a654dae7c84c6a24295ca2f930c9eb7d465fb85
Maksim <tobokerowl69@gmail.com>
2025-07-01 14:23:51 +0300

    addnew XP

--- W6/D2/XP_W6-D2.sql
+++ W6/D2/XP_W6-D2.sql
@@ -4,39 +4,39 @@
 -- SELECT * FROM items ORDER BY price
 
 --2
--- SELECT * FROM items 
+-- SELECT * FROM items
 -- WHERE price >= 80
--- OrdEr BY price ASC
+-- Order BY price ASC
 
 --3
 -- SELECT first_name, last_name FROM customers
 -- order by first_name ASC
 
---4 
--- SELECT last_name FROM customers 
+--4
+-- SELECT last_name FROM customers
 -- ORDER BY last_name DESC
 
 -- 🌟 Exercise 2 : dvdrental database
 
---1 
+--1
 -- SELECT * FROM customer
 
---2 
+--2
 -- SELECT first_name ||' '|| last_name AS full_name FROM customer
 
---3 
+--3
 -- SELECT DISTINCT create_date FROM customer
 
 --4
 -- SELECT * FROM customer
 -- ORDER BY first_name DESC
 
---5 
+--5
 -- SELECT film_id, title, description, release_year, rental_rate
 -- FROM film
 -- ORDER BY rental_rate ASC
 
---6 
+--6
 -- SELECT address, phone FROM address
 -- WHERE district = 'Texas'
 
@@ -44,15 +44,15 @@
 -- SELECT * FROM film
 -- WHERE film_id = 15 OR film_id = 150
 
---8 
+--8
 -- SELECT film_id, title, description, length, rental_rate FROM film
 -- WHERE title = 'The Matrix'
 
---9 
+--9
 -- SELECT film_id, title, description, length, rental_rate FROM film
 -- WHERE title LIKE ('Th%')
 
---10 
+--10
 -- SELECT * FROM film
 -- ORDER BY rental_rate LIMIT(10)
 
@@ -63,18 +63,18 @@
 -- OFFSET (10)
 
 --12
--- SELECT 
+-- SELECT
 -- 	customer.customer_id,
---     customer.first_name, 
---     customer.last_name, 
---     payment.amount, 
+--     customer.first_name,
+--     customer.last_name,
+--     payment.amount,
 --     payment.payment_date
 -- FROM customer
--- INNER JOIN payment ON customer.customer_id = payment.customer_id
+-- JOIN payment ON customer.customer_id = payment.customer_id
 -- ORDER BY customer.customer_id ASC
 
 --13
--- SELECT 
+-- SELECT
 -- film.film_id,
 -- film.title
 -- FROM film
@@ -87,16 +87,19 @@
 -- country.country_id,
 -- country.country,
 -- city.city
--- FROM country 
+-- FROM country
 -- JOIN city ON country.country_id = city.country_id
 -- ORDER BY country.country, city.city
 
 --15
-SELECT
-customer.customer_id,
-customer.first_name || ' ' || customer.last_name AS name,
-payment.amount,
-payment.payment_date
-FROM customer
-JOIN payment ON customer.customer_id = payment.customer_id
-ORDER BY payment.staff_id
+-- SELECT
+-- customer.customer_id,
+-- customer.first_name || ' ' || customer.last_name AS name,
+-- payment.amount,
+-- payment.payment_date
+-- FROM customer
+-- JOIN payment ON customer.customer_id = payment.customer_id
+-- ORDER BY payment.staff_id
+
+
+ select * from customer
---
+++ W6/D3/XP_W6-D3.sql
@@ -0,0 +1,165 @@
+-- 🌟 Exercise 1: DVD Rental
+--1
+-- SELECT *FROM labguage
+--2
+-- SELECT
+-- 	film.title,
+-- 	film.description,
+-- 	language.name AS language
+-- FROM
+-- 	film
+-- 	JOIN language ON film.language_id = language.language_id
+--3	
+-- SELECT
+-- 	film.title,
+-- 	film.description,
+-- 	language.name AS language
+-- FROM
+-- 	film
+-- 	RIGHT JOIN language ON film.language_id = language.language_id
+--4
+-- CREATE TABLE new_film (id serial PRIMARY KEY, name VARCHAR(50))
+-- INSERT INTO new_film 
+-- VALUES
+-- 	(1, 'The Matrix')
+--	(2,'Naruto')
+--5
+-- CREATE TABLE customer_review
+-- (
+-- review_id SERIAL NOT NULL PRIMARY KEY,
+-- film_id INTEGER NOT NULL REFERENCES new_film(id) ON DELETE CASCADE,
+-- language_id INTEGER NOT NULL REFERENCES language(language_id) ON DELETE CASCADE,
+-- title varchar(100) NOT NULL,
+-- score integer NOT NULL CHECK (score BETWEEN 1 AND 10),
+-- review_text TEXT NOT NULL,
+-- last_update TIMESTAMP NOT NULL
+-- )
+--6
+-- INSERT INTO customer_review 
+-- VALUES
+-- (
+-- 1,1,1, 'A Groundbreaking Sci-Fi Classic', 8, 
+-- 'The Matrix changed the way we think about action and reality. An unforgettable blend of philosophy, style, and groundbreaking special effects. Absolutely a must-watch!',
+-- '2025/06/30'
+-- ),
+-- (
+-- 2,2,3, 'An Inspiring Journey of Friendship and Growth', 7,
+-- 'Naruto is more than just a shonen anime—it’s a story about never giving up, friendship, and overcoming your own limits. Full of emotional moments and epic battles!',
+-- '2025/06/30'
+-- )
+
+--7
+	-- delete from new_film WHERE name = 'Naruto'
+--in the costumer_review table this film also was delited, because we used ON DELETE CASCADE 
+
+
+
+--🌟 Exercise 2 : DVD Rental
+
+--1
+
+-- UPDATE film
+-- SET language_id = 2
+-- WHERE title = 'Chamber Italian'
+
+-- UPDATE film
+-- SET language_id = 3
+-- WHERE title = 'Japanese Run'
+
+--2
+--for cuctomer table defined are 'store_id','address_id'
+
+--When you INSERT a row into a table with a FOREIGN KEY, 
+--the referenced value must already exist in the parent table.
+--If not, the insert will fail with an error.
+
+--3
+-- drop table customer_review
+-- this table is easy deleted, because we used cascade function
+-- select * from customer_review
+
+--4
+-- SELECT
+-- 	COUNT(*) AS not_returned
+-- FROM
+-- 	rental
+-- WHERE
+-- 	return_date IS NULL
+
+--5
+
+-- SELECT
+--     film.title,
+--     film.rental_rate
+-- FROM film
+-- JOIN inventory ON film.film_id = inventory.film_id
+-- JOIN rental ON inventory.inventory_id = rental.inventory_id
+-- WHERE rental.return_date IS NULL
+-- GROUP BY
+--     film.film_id, film.title, film.rental_rate
+-- ORDER BY
+--     film.rental_rate DESC
+-- LIMIT 30
+
+
+--**6**
+
+--1
+-- SELECT title FROM film
+-- 	JOIN film_actor ON film.film_id = film_actor.film_id
+-- 	JOIN actor ON film_actor.actor_id = actor.actor_id
+-- WHERE
+-- 	actor.first_name = 'Penelope'
+-- 	AND actor.last_name = 'Monroe'
+	
+--2
+-- SELECT
+-- 	title,
+-- 	rating,
+-- 	c.name AS category
+-- FROM film
+-- 	JOIN film_category fc ON film.film_id = fc.film_id
+-- 	JOIN category c ON fc.category_id = c.category_id
+-- WHERE c.name = 'Documentary' AND rating = 'R'
+
+--3
+
+-- SELECT
+-- 	first_name,
+-- 	last_name,
+-- 	amount,
+-- 	rental.return_date
+-- FROM
+-- 	customer
+-- 	JOIN payment ON customer.customer_id = payment.customer_id
+-- 	JOIN rental ON payment.rental_id = rental.rental_id
+-- WHERE
+-- 	first_name = 'Matthew'
+-- 	AND last_name = 'Mahan'
+-- 	AND return_date BETWEEN '2005-07-28' AND '2005-08-01';
+
+-- if use this parametrs, there is no 4.00$ values.
+
+--4
+
+
+-- SELECT
+-- 	title,
+-- 	description,
+-- 	first_name,
+-- 	last_name,
+-- 	amount,
+-- 	rental.return_date
+-- FROM
+-- 	customer
+-- 	JOIN payment ON customer.customer_id = payment.customer_id
+-- 	JOIN rental ON payment.rental_id = rental.rental_id
+-- 	JOIN inventory i ON rental.inventory_id = i.inventory_id
+-- 	JOIN film ON i.film_id = film.film_id
+
+-- WHERE
+-- 	first_name = 'Matthew'
+-- 	AND last_name = 'Mahan'
+-- 	AND description ILIKE '%boat%'
+-- 	OR title ILIKE '%boat%'
+	
